71ab05463600eaeffd1da3170914cbfe3b367d6a
stdEndian <010flower010@gmail.com>
2024-08-26 10:02:13 +0500

    init commit

---
+++ install.sh
@@ -0,0 +1,2 @@
+echo y | xbps-install -Su
+echo y | xbps-install terminus-font telegram-desktop mesa-dri xorg fluxbox vulkan-loader amdvlk mesa-vulkan-radeon mesa-vaapi mesa-vdpau pulseaudio chromium pavucontrol emacs-gtk3 cpupower
